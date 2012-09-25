73a0a62a0ec5995f5390eba758c8ca4efbe6b970
th2 <github6489@macronode.com>
2012-09-25 20:29:54 +0200

    added module ScreenColor

---
+++ java/src/modules/led/ScreenColor.java
@@ -0,0 +1,106 @@
+package modules.led;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.HeadlessException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+import output.LEDOutput;
+import modules.Module;
+
+public class ScreenColor implements Runnable, Module {
+
+	private LEDOutput output;
+	private boolean stop;
+	private boolean pause;
+	private int refreshInterval;
+
+	/**
+	 * Change color depending on the average color of the pixels on the screen.
+	 * Default refresh interval is 100 milliseconds.
+	 * 
+	 * @param output
+	 *            The object to send the output to.
+	 */
+	public ScreenColor(LEDOutput output) {
+		this(output, 100);
+	}
+
+	/**
+	 * Change color depending on the average color of the pixels on the screen.
+	 * 
+	 * @param output
+	 *            The object to send the output to.
+	 * @param refreshInterval
+	 *            How many milliseconds the thread should wait before the next
+	 *            refresh. default is 100.
+	 */
+	public ScreenColor(LEDOutput output, int refreshInterval) {
+		this.output = output;
+		this.refreshInterval = refreshInterval;
+		stop = false;
+		pause = false;
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (!stop) {
+				if (pause)
+					Thread.sleep(refreshInterval);
+				else {
+					BufferedImage bi = new Robot()
+							.createScreenCapture(new Rectangle(Toolkit
+									.getDefaultToolkit().getScreenSize()));
+					int r = 0, g = 0, b = 0;
+					int numberOfPixels = 0;
+					for (int x = 0; x < bi.getWidth(); x += 4) {
+						for (int y = 0; y < bi.getHeight(); y += 4) {
+							int pixel = bi.getRGB(x, y);
+							r = r + (int) (255 & (pixel >> 16)); // add up red
+							g = g + (int) (255 & (pixel >> 8)); // add up green
+							b = b + (int) (255 & (pixel)); // add up blue
+							numberOfPixels++;
+						}
+					}
+
+					r = r / numberOfPixels;
+					g = g / numberOfPixels;
+					b = b / numberOfPixels;
+					output.sendRGB(r, g, b);
+
+					Thread.sleep(refreshInterval);
+				}
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (HeadlessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void stop() {
+		stop = true;
+	}
+
+	@Override
+	public void resume() {
+		pause = false;
+	}
+
+	@Override
+	public void pause() {
+		pause = true;
+	}
+}
